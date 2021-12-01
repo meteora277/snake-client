3e6b8999314d50610fcb8dbc54847b5e0fa1a7f4
meteora277 <justin.s.diaz@gmail.com>
2021-12-01 15:40:09 -0500

    moved handleInput to play.js

--- client.js
+++ client.js
@@ -30,21 +30,3 @@ module.exports = {
   connect
 };
 
-const setupInput = function() {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput());
-
-  return stdin;
-
-};
-const handleUserInput = function(data) {
-
-  if (data === '\u0003') {
-    process.exit();
-  }
-
-};
--- play.js
+++ play.js
@@ -2,4 +2,23 @@ const { connect } = require("./client");
 
 console.log("Connecting ...");
 
+const setupInput = function() {
+
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput());
+
+  return stdin;
+
+};
+const handleUserInput = function(data) {
+
+  if (data === '\u0003') {
+    process.exit();
+  }
+
+};
+
 connect();
