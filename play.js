c9d4d9b28f3320a41b96bad0359646832b427cc0
meteora277 <justin.s.diaz@gmail.com>
2021-12-01 15:48:25 -0500

    imported setUpInput

--- play.js
+++ play.js
@@ -1,24 +1,7 @@
 const { connect } = require("./client");
+const { setupInput } = require("./input");
 
 console.log("Connecting ...");
 
-const setupInput = function() {
-
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  stdin.on('data', handleUserInput());
-
-  return stdin;
-
-};
-const handleUserInput = function(data) {
-
-  if (data === '\u0003') {
-    process.exit();
-  }
-
-};
-
 connect();
+setupInput();
